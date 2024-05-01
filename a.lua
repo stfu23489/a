b1c20c1aa42f0d0f93a5ca44e0b266a1bd17fc54
stfu23489 <95998301+stfu23489@users.noreply.github.com>
2024-05-01 18:26:10 +1000

    Update a.lua

--- a.lua
+++ a.lua
@@ -919,6 +919,7 @@ function API:killall(TeamToKill)
 			game:GetService("ReplicatedStorage").ShootEvent:FireServer(BulletTable, Gun)
 		end)
 		repeat API:swait() workspace.Remote.TeamEvent:FireServer("Bright orange") until game.Players.LocalPlayer.TeamColor.Name == "Bright orange"
+		wait(.4)
 		API:GetGun("M9")
 		repeat API:swait() Gun = Player.Backpack:FindFirstChild("M9") or Player.Character:FindFirstChild("M9") until Gun
 		local Gun = Player.Backpack:FindFirstChild("M9") or Player.Character:FindFirstChild("M9")
